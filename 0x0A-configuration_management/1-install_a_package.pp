53a2fafe1241c76cb334a5b64c3ab058a8e46d1b
brahim bouchikha <brahim.bouchikha@gmail.com>
2024-06-29 00:47:28 +0100

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
-# install_flask.pp
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/bin', '/usr/bin'],
-  unless  => '/usr/bin/pip3 show flask | grep "Version: 2.1.0"',
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
