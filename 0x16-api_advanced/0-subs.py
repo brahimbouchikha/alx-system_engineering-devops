618bcf05a979269ad2355597e6dc31d8817ad332
brahim bouchikha <brahim.bouchikha@gmail.com>
2024-08-14 01:49:38 +0100

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,9 +9,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ return number of subscribers """
-    url = "https://www.reddit.com/r/{}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "brahimbouchikha"}
-    response = requests.get(url.format(subreddit), headers=headers)
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
         return response.json().get("data").get("subscribers")
     else:
