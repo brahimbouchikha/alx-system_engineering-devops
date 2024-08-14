ae53f24cb9986fc7752b8fbb37251d652dd47140
brahim bouchikha <brahim.bouchikha@gmail.com>
2024-08-14 01:48:04 +0100

    0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+function that queries Reddit API and returns
+number of subscribers (not active users, total subscribers)
+"""
+import json
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ return number of subscribers """
+    url = "https://www.reddit.com/r/{}/about.json"
+    headers = {"User-Agent": "brahimbouchikha"}
+    response = requests.get(url.format(subreddit), headers=headers)
+    if response.status_code == 200:
+        return response.json().get("data").get("subscribers")
+    else:
+        return 0
