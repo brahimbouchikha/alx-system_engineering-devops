cd7b6c6ad81954a4e938e2e4cb6a88c04fba86c3
brahim bouchikha <brahim.bouchikha@gmail.com>
2024-08-14 01:52:19 +0100

    0

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,7 +10,7 @@ import requests
 def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "brahimbouchikha)"
     }
     params = {
         "limit": 10
