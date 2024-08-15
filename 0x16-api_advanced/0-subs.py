30153953fde43a1434a0ca4cfa85f592f6de27b2
brahim bouchikha <brahim.bouchikha@gmail.com>
2024-08-15 14:57:13 +0100

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,7 +14,7 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        return data.get('data').get('subscribers')
-
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
         return 0
