51795e1298ac935a7ae3fa684a151338fa5c3378
brahim bouchikha <brahim.bouchikha@gmail.com>
2024-08-15 14:45:00 +0100

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,13 +8,18 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """ return number of subscribers """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        return data.get('data').get('subscribers')
+    """
+    Function that queries the Reddit API and returns
+    the number of subscribers for a given subreddit.
+    """
 
+    res = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Mozilla/5.0"},
+        allow_redirects=False
+    )
+    if res.status_code == 200:
+        data = res.json()
+        return data.get('data').get('subscribers')
     else:
         return 0
