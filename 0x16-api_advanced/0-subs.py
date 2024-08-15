4f5a67b77593d6f207236a2875b10c292a123980
brahim bouchikha <brahim.bouchikha@gmail.com>
2024-08-15 14:43:51 +0100

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,17 +3,18 @@
 function that queries Reddit API and returns
 number of subscribers (not active users, total subscribers)
 """
-import json
+
 import requests
 
 
 def number_of_subscribers(subreddit):
     """ return number of subscribers """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "brahimbouchikha"}
-    response = requests.get(url, headers=headers)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        return response.json().get("data").get("subscribers")
-    else:
-       return "OK"
+        data = response.json()
+        return data.get('data').get('subscribers')
 
+    else:
+        return 0
