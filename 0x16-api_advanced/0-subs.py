0a192a7f20aa637358e654e86f94c7899d553753
brahim bouchikha <brahim.bouchikha@gmail.com>
2024-08-15 14:48:23 +0100

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,13 +13,13 @@ def number_of_subscribers(subreddit):
     the number of subscribers for a given subreddit.
     """
 
-    res = requests.get(
+    respoonse = requests.get(
         "https://www.reddit.com/r/{}/about.json".format(subreddit),
         headers={"User-Agent": "Mozilla/5.0"},
         allow_redirects=False
     )
-    if res.status_code == 200:
-        data = res.json()
-        return data.get('data').get('subscribers')
-    else:
+    if respoonse.status_code == 404:
         return 0
+    else:
+        data = respoonse.json()
+        return data.get('data').get('subscribers')
